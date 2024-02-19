e80142d93ffcf3f8123a657d5ca7290e8263c3aa
okbrandon <okbrandonlooksdumb@gmail.com>
2024-02-19 10:06:34 +0100

    :adhesive_bandage: Replaced static website

--- srcs/requirements/bonus/static/tools/ws_installer.sh
+++ srcs/requirements/bonus/static/tools/ws_installer.sh
@@ -6,22 +6,14 @@
 #    By: bsoubaig <bsoubaig@student.42nice.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/11/19 18:04:50 by bsoubaig          #+#    #+#              #
-#    Updated: 2023/11/19 18:12:45 by bsoubaig         ###   ########.fr        #
+#    Updated: 2024/02/19 09:59:22 by bsoubaig         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
-if [ ! -d "/app/public" ]; then
-	
-	mkdir /app/public
-
-	# Scrapping website
-	echo "[INFO] Scrapping website..."
-	git clone https://github.com/okbrandon/brandoncodes.dev.git /app/public > /dev/null 2>&1
-	chmod -R 755 /app/public
-	
-fi
+echo "[INFO] Adjusting permissions..."
+chmod -R 755 /app/public
 
 # Start server
 echo "[INFO] Starting server..."
